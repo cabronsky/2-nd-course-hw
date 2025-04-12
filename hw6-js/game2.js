aa1724d73b1afcc9be787578898367e36c5c4399
cabronsky <woodooband@gmail.com>
2025-04-12 14:48:08 +0100

    finished

--- hw6-js/game2.js
+++ hw6-js/game2.js
@@ -1,18 +1,34 @@
-function guessNumberGame() {
-    const number = 88;
-    let userGuess;
-
-    while (userGuess !== number) {
-        userGuess = Number(prompt("Угадайте число от 1 до 100:"));
-
-        if (userGuess < number) {
-            alert("Загаданное число больше.");
-        } else if (userGuess > number) {
-            alert("Загаданное число меньше.");
-        } else {
-            alert("Поздравляю! Вы угадали число!");
-        }
+function generateQuestion() {
+    const operations = ['+', '-', '*', '/'];
+    const operation = operations[Math.floor(Math.random() * operations.length)];
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
+    let question = '';
+    let correctAnswer;
+    if (operation === '+') {
+        question = `${num1} + ${num2}`;
+        correctAnswer = num1 + num2;
+      } else if (operation === '-') {
+        question = `${num1} - ${num2}`;
+        correctAnswer = num1 - num2;
+      } else if (operation === '*') {
+        question = `${num1} * ${num2}`;
+        correctAnswer = num1 * num2;
+      } else if (operation === '/') {
+        question = `${num1 * num2} / ${num2}`; 
+        correctAnswer = num1;
+      }
+    
+      return { question, correctAnswer };
     }
-}
-
-guessNumberGame();
+    function startGame() {
+        const { question, correctAnswer } = generateQuestion();
+        const userAnswer = parseFloat(prompt(`Решите задачу: ${question}`));
+        if (userAnswer === correctAnswer) {
+            alert("Верно! Молодец!");
+          } else {
+            alert(`Неверно! Правильный ответ: ${correctAnswer}`);
+          }
+    }
+    startGame();
+        
