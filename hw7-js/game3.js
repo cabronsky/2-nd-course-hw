65e59bee9090a58c33ec9c340a41fdced6b21bb6
cabronsky <woodooband@gmail.com>
2025-04-14 15:49:33 +0100

    finished games

--- hw7-js/game3.js
+++ hw7-js/game3.js
@@ -1,34 +1,8 @@
-function generateQuestion() {
-    const operations = ['+', '-', '*', '/'];
-    const operation = operations[Math.floor(Math.random() * operations.length)];
-    const num1 = Math.floor(Math.random() * 10) + 1;
-    const num2 = Math.floor(Math.random() * 10) + 1;
-    let question = '';
-    let correctAnswer;
-    if (operation === '+') {
-        question = `${num1} + ${num2}`;
-        correctAnswer = num1 + num2;
-      } else if (operation === '-') {
-        question = `${num1} - ${num2}`;
-        correctAnswer = num1 - num2;
-      } else if (operation === '*') {
-        question = `${num1} * ${num2}`;
-        correctAnswer = num1 * num2;
-      } else if (operation === '/') {
-        question = `${num1 * num2} / ${num2}`; 
-        correctAnswer = num1;
-      }
-    
-      return { question, correctAnswer };
-    }
-    function startGame() {
-        const { question, correctAnswer } = generateQuestion();
-        const userAnswer = parseFloat(prompt(`Решите задачу: ${question}`));
-        if (userAnswer === correctAnswer) {
-            alert("Верно! Молодец!");
-          } else {
-            alert(`Неверно! Правильный ответ: ${correctAnswer}`);
-          }
-    }
-    startGame();
-        
+function reverseTextGame() {
+  const userInput = prompt("Введи текст:");
+  const upperCaseInput = userInput.toUpperCase();
+  const reversed = upperCaseInput.split('').reverse().join('');
+  alert("Перевёрнутый текст: " + reversed);
+}
+
+reverseTextGame();
